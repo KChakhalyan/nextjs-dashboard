a0d38bdb7d7a524d0c305f65cb0a96200926b9df
KChakhalyan <karenchakhalyan23@gmail.com>
2023-12-14 01:21:02 +0400

    implemented search and pagination using URL Search Params and Next.js APIs

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,25 @@
 'use client';
-
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams); // Create a new URLSearchParams object
+    params.set('page', '1'); // Reset page to 1
+
+    if (term) {
+      // Check if term is truthy
+      params.set('query', term); // Set query to term
+    } else {
+      params.delete('query'); // Remove query if term is falsy
+    }
+    replace(`${pathName}?${params.toString()}`); // Update the URL with the new query
+  }, 300); // Delay in milliseconds
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -10,6 +27,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
         placeholder={placeholder}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
